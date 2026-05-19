4637eafee528310fa23859ebb43f142c6ae29ec4
Sunny-OpenLab <18609311366@163.com>
2026-05-19 11:30:45 +0800

    Update args.py

--- args.py
+++ args.py
@@ -1,19 +1,8 @@
-# -*-coding:utf-8 -*-
-
-"""
-# File       : args.py
-# Time       ：2025/9/5 10:41
-# Author     ：Qiang42
-# version    ：python 3.8
-# Description：
-"""
-
 import os
 import argparse
 import platform
 
-# DATASETNAME = "MSRS_240_320"
-DATASETNAME_VS = "small-HDO-X150" # HDO | KAIST | M3SVD
+DATASETNAME_VS = "M3SVD"
 
 system_name = platform.system()
 
@@ -26,14 +15,9 @@ def parse_args(timestamp_date, timestamp_time):
     parser.add_argument('--NEW', type=bool, default=True)
     parser.add_argument('--model', type=str, default='OFVFusion')
     parser.add_argument('--seed', type=int, default=1016)
-
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument("--early_stopping_patience", type=int, default=5)
-
     parser.add_argument('--learning_rate', type=float, default=1e-4)
-    # parser.add_argument('--T_0', type=float, default=10)
-    # parser.add_argument('--T_mult', type=int, default=2)
-    # parser.add_argument('--eta_min', type=float, default=1e-6)
     parser.add_argument("--lr_patience", type=int, default=1)
     parser.add_argument("--lr_factor", type=float, default=0.5)
     parser.add_argument('--float16', type=bool, default=False)
@@ -44,22 +28,20 @@ def parse_args(timestamp_date, timestamp_time):
         parser.add_argument('--batch_size', type=int, default=2)
         parser.add_argument('--workers', type=int, default=0)
         parser.add_argument("--dataset_path_VS", type=str, default=r"F:/DATASETS/IVIF/data/" + DATASETNAME_VS + ".tar")
-        parser.add_argument("--test_dataset_path_VS", type=str, default=r"F:\DATASETS\IVIF\test_data") #F:\PythonProject\IVIF\test_data_M3SVD | F:/DATASETS/IVIF/test_data/
+        parser.add_argument("--test_dataset_path_VS", type=str, default=r"F:\DATASETS\IVIF\test_data") 
         parser.add_argument("--test_Video_path", type=str, default=r"./video_test.tar")
         parser.add_argument("--system_name", type=str, default="Windows")
-        # parser.add_argument("--show_num", type=int, default=1)
-        parser.add_argument("--indices", type=int, default=10)  # 子数据集缩小倍数
+        parser.add_argument("--indices", type=int, default=10) 
         parser.add_argument("--test_video_path", type=str, default=r"F:/DATASETS/IVIF/video_test/")
 
     elif system_name == "Linux":
         print("Linux")
-        parser.add_argument('--batch_size', type=int, default=4) # 128 |  64 | 32 | 16
+        parser.add_argument('--batch_size', type=int, default=4) 
         parser.add_argument('--workers', type=int, default=25)
         parser.add_argument("--dataset_path_VS", type=str, default=r"/root/autodl-fs/" + DATASETNAME_VS + ".tar")
         parser.add_argument("--test_dataset_path_VS", type=str, default=r"/root/autodl-fs/test_data/")
         parser.add_argument("--system_name", type=str, default="Linux")
-        # parser.add_argument("--show_num", type=int, default=10)
-        parser.add_argument("--indices", type=int, default=1)  # 子数据集缩小倍数
+        parser.add_argument("--indices", type=int, default=1)  
         parser.add_argument('--shutdown', type=bool, default=True)
         parser.add_argument("--test_video_path", type=str, default=r"/root/autodl-fs/video_test/")
 
